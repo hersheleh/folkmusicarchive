eb5829042d3541f015a6050cc0eb4fbe6d97dc23
Stan Dyro <stand@pk4media.com>
2013-01-13 23:38:11 -0800

    song page mockup, jade templates

--- folk.py
+++ folk.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template, redirect
 
-
 app = Flask(__name__)
+
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.jade')
 
-@app.route('/song_editor')
+@app.route('/song')
 def song_editor():
-    return render_template('song_editor.html')
+    return render_template('song.jade')
 
+@app.route('/browse')
+def browse():
+    return render_template('browse.jade')
+
+@app.route('/discuss')
+def discuss():
+    return render_template('discuss.jade')
 
 if __name__ == '__main__':
     app.debug = True
